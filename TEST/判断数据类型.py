1854c43406739d5ebb48e4191f7b4ba916fa346f
Duyuefan <17600075667@163.com>
2023-04-10 11:52:49 +0800

    123

---
+++ TEST/判断数据类型.py
@@ -0,0 +1,6 @@
+float_1 = 1.2
+print(type(float_1))
+dict_1 = {'name':'sb'}
+print(type(dict_1))
+tuple_1 = (1,2,3)
+print(type(tuple_1))
---
+++ TEST/数据类型转换.py
@@ -0,0 +1,4 @@
+a = True
+print(int(a))
+b = False
+print(int(b))
