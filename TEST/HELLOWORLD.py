e7ddb2459bfaf9632523a655663b3f3be65af17d
Duyuefan <17600075667@163.com>
2023-04-10 11:32:35 +0800

    Initial commit

---
+++ TEST/HELLOWORLD.py
@@ -0,0 +1,9 @@
+import time
+from flask import Flask
+app = Flask(__name__)
+@app.route("/hello")
+def hello():
+    time.sleep(5)
+    return "Hello World"
+if __name__ == "__main__":
+    app.run()
